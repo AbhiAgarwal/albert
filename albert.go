7cc7af7b3db95017aa24e41582ae4aacc356c505
Abhi Agarwal <me@abhiagarwal.com>
2015-04-27 15:32:41 -0400

    Just adding JSON struct

--- albert.go
+++ albert.go
@@ -27,117 +27,117 @@ type SSR_GET_CLASSES_REQ struct {
 
 type SSRGETCLASSESRESP struct {
 	SEARCHRESULT struct {
-		ERRORWARNTEXT   string `xml:"ERROR_WARN_TEXT"`
-		SSRCLASSCOUNT   string `xml:"SSR_CLASS_COUNT"`
-		SSRCOURSECOUNT  string `xml:"SSR_COURSE_COUNT"`
-		SSRERRLMTEXCEED string `xml:"SSR_ERR_LMT_EXCEED"`
-		SSRWRNLMTEXCEED string `xml:"SSR_WRN_LMT_EXCEED"`
+		ERRORWARNTEXT   string `xml:"ERROR_WARN_TEXT" json:"ERROR_WARN_TEXT"`
+		SSRCLASSCOUNT   string `xml:"SSR_CLASS_COUNT" json:"SSR_CLASS_COUNT"`
+		SSRCOURSECOUNT  string `xml:"SSR_COURSE_COUNT" json:"SSR_COURSE_COUNT"`
+		SSRERRLMTEXCEED string `xml:"SSR_ERR_LMT_EXCEED" json:"SSR_ERR_LMT_EXCEED"`
+		SSRWRNLMTEXCEED string `xml:"SSR_WRN_LMT_EXCEED" json:"SSR_WRN_LMT_EXCEED"`
 		SUBJECTS        struct {
 			SUBJECT []struct {
-				ACADCAREER         string `xml:"ACAD_CAREER"`
-				ACADCAREERLOVDescr string `xml:"ACAD_CAREER_LOVDescr"`
-				CATALOGNBR         string `xml:"CATALOG_NBR"`
+				ACADCAREER         string `xml:"ACAD_CAREER" json:"ACAD_CAREER"`
+				ACADCAREERLOVDescr string `xml:"ACAD_CAREER_LOVDescr" json:"ACAD_CAREER_LOVDescr"`
+				CATALOGNBR         string `xml:"CATALOG_NBR" json:"CATALOG_NBR"`
 				CLASSESSUMMARY     struct {
 					CLASSSUMMARY struct {
-						CAMPUS                 string `xml:"CAMPUS"`
-						CAMPUSLOVDescr         string `xml:"CAMPUS_LOVDescr"`
-						CATALOGNBR             string `xml:"CATALOG_NBR"`
+						CAMPUS                 string `xml:"CAMPUS" json:"CAMPUS"`
+						CAMPUSLOVDescr         string `xml:"CAMPUS_LOVDescr" json:"CAMPUS_LOVDescr"`
+						CATALOGNBR             string `xml:"CATALOG_NBR" json:"CATALOG_NBR"`
 						CLASSESMEETINGPATTERNS struct {
 							CLASSMEETINGPATTERN struct {
-								BLDGCD           string `xml:"BLDG_CD"`
+								BLDGCD           string `xml:"BLDG_CD" json:"BLDG_CD"`
 								CLASSINSTRUCTORS struct {
 									CLASSINSTRUCTOR struct {
-										CLASSMTGNBR             string `xml:"CLASS_MTG_NBR"`
-										CLASSSECTION            string `xml:"CLASS_SECTION"`
-										CRSEID                  string `xml:"CRSE_ID"`
-										CRSEOFFERNBR            string `xml:"CRSE_OFFER_NBR"`
-										EMPLID                  string `xml:"EMPLID"`
-										ENCRYPTEDEMPLID         string `xml:"ENCRYPTED_EMPLID"`
-										FIRSTNAME               string `xml:"FIRST_NAME"`
-										INSTRROLE               string `xml:"INSTR_ROLE"`
-										INSTRROLELOVDescr       string `xml:"INSTR_ROLE_LOVDescr"`
-										LASTNAME                string `xml:"LAST_NAME"`
-										NAMEDISPLAY             string `xml:"NAME_DISPLAY"`
-										SCHEDPRINTINSTR         string `xml:"SCHED_PRINT_INSTR"`
-										SCHEDPRINTINSTRLOVDescr string `xml:"SCHED_PRINT_INSTR_LOVDescr"`
-										SESSIONCODE             string `xml:"SESSION_CODE"`
-										SESSIONCODELOVDescr     string `xml:"SESSION_CODE_LOVDescr"`
-										STRM                    string `xml:"STRM"`
-										STRMLOVDescr            string `xml:"STRM_LOVDescr"`
-									} `xml:"CLASS_INSTRUCTOR"`
-								} `xml:"CLASS_INSTRUCTORS"`
-								CLASSMTGNBR         string `xml:"CLASS_MTG_NBR"`
-								CLASSSECTION        string `xml:"CLASS_SECTION"`
-								CRSEID              string `xml:"CRSE_ID"`
-								CRSEOFFERNBR        string `xml:"CRSE_OFFER_NBR"`
-								CRSTOPICID          string `xml:"CRS_TOPIC_ID"`
-								CRSTOPICIDLOVDescr  string `xml:"CRS_TOPIC_ID_LOVDescr"`
-								DESCR               string `xml:"DESCR"`
-								ENDDT               string `xml:"END_DT"`
-								FACILITYID          string `xml:"FACILITY_ID"`
-								FACILITYIDLOVDescr  string `xml:"FACILITY_ID_LOVDescr"`
-								FRI                 string `xml:"FRI"`
-								MEETINGTIMEEND      string `xml:"MEETING_TIME_END"`
-								MEETINGTIMESTART    string `xml:"MEETING_TIME_START"`
-								MON                 string `xml:"MON"`
-								SAT                 string `xml:"SAT"`
-								SCCLATITUDE         string `xml:"SCC_LATITUDE"`
-								SCCLONGITUDE        string `xml:"SCC_LONGITUDE"`
-								SESSIONCODE         string `xml:"SESSION_CODE"`
-								SESSIONCODELOVDescr string `xml:"SESSION_CODE_LOVDescr"`
-								SSRINSTRLONG        string `xml:"SSR_INSTR_LONG"`
-								SSRMTGDTLONG        string `xml:"SSR_MTG_DT_LONG"`
-								SSRMTGLOCLONG       string `xml:"SSR_MTG_LOC_LONG"`
-								SSRMTGSCHEDLONG     string `xml:"SSR_MTG_SCHED_LONG"`
-								SSRTOPICLONG        string `xml:"SSR_TOPIC_LONG"`
-								STARTDT             string `xml:"START_DT"`
-								STNDMTGPAT          string `xml:"STND_MTG_PAT"`
-								STRM                string `xml:"STRM"`
-								SUN                 string `xml:"SUN"`
-								THURS               string `xml:"THURS"`
-								TUES                string `xml:"TUES"`
-								WED                 string `xml:"WED"`
-							} `xml:"CLASS_MEETING_PATTERN"`
-						} `xml:"CLASSES_MEETING_PATTERNS"`
-						CLASSNBR                string   `xml:"CLASS_NBR"`
-						CLASSSECTION            string   `xml:"CLASS_SECTION"`
-						CLASSTOPIC              string   `xml:"CLASS_TOPIC"`
-						COMBINEDSECTION         string   `xml:"COMBINED_SECTION"`
-						COMBINEDSECTIONLOVDescr string   `xml:"COMBINED_SECTION_LOVDescr"`
-						CRSEID                  string   `xml:"CRSE_ID"`
-						CRSEIDLOVDescr          string   `xml:"CRSE_ID_LOVDescr"`
-						CRSEOFFERNBR            string   `xml:"CRSE_OFFER_NBR"`
-						INSTITUTION             string   `xml:"INSTITUTION"`
-						INSTRUCTIONMODE         string   `xml:"INSTRUCTION_MODE"`
-						INSTRUCTIONMODELOVDescr string   `xml:"INSTRUCTION_MODE_LOVDescr"`
-						SCHEDULEPRINT           string   `xml:"SCHEDULE_PRINT"`
-						SCHEDULEPRINTLOVDescr   string   `xml:"SCHEDULE_PRINT_LOVDescr"`
-						SESSIONCODE             string   `xml:"SESSION_CODE"`
-						SESSIONCODELOVDescr     string   `xml:"SESSION_CODE_LOVDescr"`
-						SSRCLASSNAMELONG        string   `xml:"SSR_CLASSNAME_LONG"`
-						SSRCOMPONENT            string   `xml:"SSR_COMPONENT"`
-						SSRCOMPONENTLOVDescr    string   `xml:"SSR_COMPONENT_LOVDescr"`
-						SSRDESCRLONG            string   `xml:"SSR_DESCRLONG"`
-						STATUS                  []string `xml:"STATUS"`
-						STATUSLOVDescr          string   `xml:"STATUS_LOVDescr"`
-						STRM                    string   `xml:"STRM"`
-						STRMLOVDescr            string   `xml:"STRM_LOVDescr"`
-						SUBJECT                 string   `xml:"SUBJECT"`
-						SUBJECTLOVDescr         string   `xml:"SUBJECT_LOVDescr"`
-					} `xml:"CLASS_SUMMARY"`
-				} `xml:"CLASSES_SUMMARY"`
-				COURSETITLELONG     string `xml:"COURSE_TITLE_LONG"`
-				CRSEID              string `xml:"CRSE_ID"`
-				CRSEIDLOVDescr      string `xml:"CRSE_ID_LOVDescr"`
-				CRSEOFFERNBR        string `xml:"CRSE_OFFER_NBR"`
-				INSTITUTION         string `xml:"INSTITUTION"`
-				INSTITUTIONLOVDescr string `xml:"INSTITUTION_LOVDescr"`
-				SUBJECT             string `xml:"SUBJECT"`
-				SUBJECTLOVDescr     string `xml:"SUBJECT_LOVDescr"`
-			} `xml:"SUBJECT"`
-		} `xml:"SUBJECTS"`
-	} `xml:"SEARCH_RESULT"`
-	Xmlns string `xml:"_xmlns"`
+										CLASSMTGNBR             string `xml:"CLASS_MTG_NBR" json:"CLASS_MTG_NBR"`
+										CLASSSECTION            string `xml:"CLASS_SECTION" json:"CLASS_SECTION"`
+										CRSEID                  string `xml:"CRSE_ID" json:"CRSE_ID"`
+										CRSEOFFERNBR            string `xml:"CRSE_OFFER_NBR" json:"CRSE_OFFER_NBR"`
+										EMPLID                  string `xml:"EMPLID" json:"EMPLID"`
+										ENCRYPTEDEMPLID         string `xml:"ENCRYPTED_EMPLID" json:"ENCRYPTED_EMPLID"`
+										FIRSTNAME               string `xml:"FIRST_NAME" json:"FIRST_NAME"`
+										INSTRROLE               string `xml:"INSTR_ROLE" json:"INSTR_ROLE"`
+										INSTRROLELOVDescr       string `xml:"INSTR_ROLE_LOVDescr" json:"INSTR_ROLE_LOVDescr"`
+										LASTNAME                string `xml:"LAST_NAME" json:"LAST_NAME"`
+										NAMEDISPLAY             string `xml:"NAME_DISPLAY" json:"NAME_DISPLAY"`
+										SCHEDPRINTINSTR         string `xml:"SCHED_PRINT_INSTR" json:"SCHED_PRINT_INSTR"`
+										SCHEDPRINTINSTRLOVDescr string `xml:"SCHED_PRINT_INSTR_LOVDescr" json:"SCHED_PRINT_INSTR_LOVDescr"`
+										SESSIONCODE             string `xml:"SESSION_CODE" json:"SESSION_CODE"`
+										SESSIONCODELOVDescr     string `xml:"SESSION_CODE_LOVDescr" json:"SESSION_CODE_LOVDescr"`
+										STRM                    string `xml:"STRM" json:"STRM"`
+										STRMLOVDescr            string `xml:"STRM_LOVDescr" json:"STRM_LOVDescr"`
+									} `xml:"CLASS_INSTRUCTOR" json:"CLASS_INSTRUCTOR"`
+								} `xml:"CLASS_INSTRUCTORS" json:"CLASS_INSTRUCTORS"`
+								CLASSMTGNBR         string `xml:"CLASS_MTG_NBR" json:"CLASS_MTG_NBR"`
+								CLASSSECTION        string `xml:"CLASS_SECTION" json:"CLASS_SECTION"`
+								CRSEID              string `xml:"CRSE_ID" json:"CRSE_ID"`
+								CRSEOFFERNBR        string `xml:"CRSE_OFFER_NBR" json:"CRSE_OFFER_NBR"`
+								CRSTOPICID          string `xml:"CRS_TOPIC_ID" json:"CRS_TOPIC_ID"`
+								CRSTOPICIDLOVDescr  string `xml:"CRS_TOPIC_ID_LOVDescr" json:"CRS_TOPIC_ID_LOVDescr"`
+								DESCR               string `xml:"DESCR" json:"DESCR"`
+								ENDDT               string `xml:"END_DT" json:"END_DT"`
+								FACILITYID          string `xml:"FACILITY_ID" json:"FACILITY_ID"`
+								FACILITYIDLOVDescr  string `xml:"FACILITY_ID_LOVDescr" json:"FACILITY_ID_LOVDescr"`
+								FRI                 string `xml:"FRI" json:"FRI"`
+								MEETINGTIMEEND      string `xml:"MEETING_TIME_END" json:"MEETING_TIME_END"`
+								MEETINGTIMESTART    string `xml:"MEETING_TIME_START" json:"MEETING_TIME_START"`
+								MON                 string `xml:"MON" json:"MON"`
+								SAT                 string `xml:"SAT" json:"SAT"`
+								SCCLATITUDE         string `xml:"SCC_LATITUDE" json:"SCC_LATITUDE"`
+								SCCLONGITUDE        string `xml:"SCC_LONGITUDE" json:"SCC_LONGITUDE"`
+								SESSIONCODE         string `xml:"SESSION_CODE" json:"SESSION_CODE"`
+								SESSIONCODELOVDescr string `xml:"SESSION_CODE_LOVDescr" json:"SESSION_CODE_LOVDescr"`
+								SSRINSTRLONG        string `xml:"SSR_INSTR_LONG" json:"SSR_INSTR_LONG"`
+								SSRMTGDTLONG        string `xml:"SSR_MTG_DT_LONG" json:"SSR_MTG_DT_LONG"`
+								SSRMTGLOCLONG       string `xml:"SSR_MTG_LOC_LONG" json:"SSR_MTG_LOC_LONG"`
+								SSRMTGSCHEDLONG     string `xml:"SSR_MTG_SCHED_LONG" json:"SSR_MTG_SCHED_LONG"`
+								SSRTOPICLONG        string `xml:"SSR_TOPIC_LONG" json:"SSR_TOPIC_LONG"`
+								STARTDT             string `xml:"START_DT" json:"START_DT"`
+								STNDMTGPAT          string `xml:"STND_MTG_PAT" json:"STND_MTG_PAT"`
+								STRM                string `xml:"STRM" json:"STRM"`
+								SUN                 string `xml:"SUN" json:"SUN"`
+								THURS               string `xml:"THURS" json:"THURS"`
+								TUES                string `xml:"TUES" json:"TUES"`
+								WED                 string `xml:"WED" json:"WED"`
+							} `xml:"CLASS_MEETING_PATTERN" json:"CLASS_MEETING_PATTERN"`
+						} `xml:"CLASSES_MEETING_PATTERNS" json:"CLASSES_MEETING_PATTERNS"`
+						CLASSNBR                string   `xml:"CLASS_NBR" json:"CLASS_NBR"`
+						CLASSSECTION            string   `xml:"CLASS_SECTION" json:"CLASS_SECTION"`
+						CLASSTOPIC              string   `xml:"CLASS_TOPIC" json:"CLASS_TOPIC"`
+						COMBINEDSECTION         string   `xml:"COMBINED_SECTION" json:"COMBINED_SECTION"`
+						COMBINEDSECTIONLOVDescr string   `xml:"COMBINED_SECTION_LOVDescr" json:"COMBINED_SECTION_LOVDescr"`
+						CRSEID                  string   `xml:"CRSE_ID" json:"CRSE_ID"`
+						CRSEIDLOVDescr          string   `xml:"CRSE_ID_LOVDescr" json:"CRSE_ID_LOVDescr"`
+						CRSEOFFERNBR            string   `xml:"CRSE_OFFER_NBR" json:"CRSE_OFFER_NBR"`
+						INSTITUTION             string   `xml:"INSTITUTION" json:"INSTITUTION"`
+						INSTRUCTIONMODE         string   `xml:"INSTRUCTION_MODE" json:"INSTRUCTION_MODE"`
+						INSTRUCTIONMODELOVDescr string   `xml:"INSTRUCTION_MODE_LOVDescr" json:"INSTRUCTION_MODE_LOVDescr"`
+						SCHEDULEPRINT           string   `xml:"SCHEDULE_PRINT" json:"SCHEDULE_PRINT"`
+						SCHEDULEPRINTLOVDescr   string   `xml:"SCHEDULE_PRINT_LOVDescr" json:"SCHEDULE_PRINT_LOVDescr"`
+						SESSIONCODE             string   `xml:"SESSION_CODE" json:"SESSION_CODE"`
+						SESSIONCODELOVDescr     string   `xml:"SESSION_CODE_LOVDescr" json:"SESSION_CODE_LOVDescr"`
+						SSRCLASSNAMELONG        string   `xml:"SSR_CLASSNAME_LONG" json:"SSR_CLASSNAME_LONG"`
+						SSRCOMPONENT            string   `xml:"SSR_COMPONENT" json:"SSR_COMPONENT"`
+						SSRCOMPONENTLOVDescr    string   `xml:"SSR_COMPONENT_LOVDescr" json:"SSR_COMPONENT_LOVDescr"`
+						SSRDESCRLONG            string   `xml:"SSR_DESCRLONG" json:"SSR_DESCRLONG"`
+						STATUS                  []string `xml:"STATUS" json:"STATUS"`
+						STATUSLOVDescr          string   `xml:"STATUS_LOVDescr" json:"STATUS_LOVDescr"`
+						STRM                    string   `xml:"STRM" json:"STRM"`
+						STRMLOVDescr            string   `xml:"STRM_LOVDescr" json:"STRM_LOVDescr"`
+						SUBJECT                 string   `xml:"SUBJECT" json:"SUBJECT"`
+						SUBJECTLOVDescr         string   `xml:"SUBJECT_LOVDescr" json:"SUBJECT_LOVDescr"`
+					} `xml:"CLASS_SUMMARY" json:"CLASS_SUMMARY"`
+				} `xml:"CLASSES_SUMMARY" json:"CLASSES_SUMMARY"`
+				COURSETITLELONG     string `xml:"COURSE_TITLE_LONG" json:"COURSE_TITLE_LONG"`
+				CRSEID              string `xml:"CRSE_ID" json:"CRSE_ID"`
+				CRSEIDLOVDescr      string `xml:"CRSE_ID_LOVDescr" json:"CRSE_ID_LOVDescr"`
+				CRSEOFFERNBR        string `xml:"CRSE_OFFER_NBR" json:"CRSE_OFFER_NBR"`
+				INSTITUTION         string `xml:"INSTITUTION" json:"INSTITUTION"`
+				INSTITUTIONLOVDescr string `xml:"INSTITUTION_LOVDescr" json:"INSTITUTION_LOVDescr"`
+				SUBJECT             string `xml:"SUBJECT" json:"SUBJECT"`
+				SUBJECTLOVDescr     string `xml:"SUBJECT_LOVDescr" json:"SUBJECT_LOVDescr"`
+			} `xml:"SUBJECT" json:"SUBJECT"`
+		} `xml:"SUBJECTS" json:"SUBJECTS"`
+	} `xml:"SEARCH_RESULT" json:"SEARCH_RESULT"`
+	Xmlns string `xml:"_xmlns" json:"_xmlns"`
 }
 
 func GetRequestHTTPS() SSRGETCLASSESRESP {
